63064212493af9a95c271ff82170db85b188ce69
tomasz-rozmus <tomasz.rozmus@gmail.com>
2020-09-22 15:32:58 +0200

    feat(fe) simpler anchoring

--- src/components/catalog/PresentationHelpers.tsx
+++ src/components/catalog/PresentationHelpers.tsx
@@ -43,14 +43,6 @@ export const RandomText: FunctionComponent<RandomTextProps> = ({
   );
 };
 
-type AnchorProps = {
-  id: string;
-};
-
-const Anchor: FunctionComponent<AnchorProps> = ({ id }) => (
-  <a href={id}>{id}</a>
-);
-
 type SectionProps = {
   header?: string;
 };
@@ -62,8 +54,7 @@ export const Section: FunctionComponent<SectionProps> = ({
   return (
     <Box mb={2}>
       <Box mb={2}>
-        <Anchor id={header} />
-        <Typography variant="h4" gutterBottom>
+        <Typography id={header} variant="h4" gutterBottom>
           {header}
         </Typography>
         {children}
