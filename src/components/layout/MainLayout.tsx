dec8f347a4c5c5db3b7fc053d1194036c98b7613
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-01-04 17:46:20 +0100

    feat(fe) tx history back

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,45 +1,68 @@
-import { Button, Container, Divider, Drawer, Grid, ListItem, useTheme, } from '@material-ui/core'
-import AppBar from '@material-ui/core/AppBar'
-import IconButton from '@material-ui/core/IconButton'
-import { makeStyles, Theme } from '@material-ui/core/styles'
-import Toolbar from '@material-ui/core/Toolbar'
-import CloseIcon from '@material-ui/icons/Close'
-import MenuIcon from '@material-ui/icons/Menu'
-import { useMultiwallet, WalletPickerModal, WalletPickerProps, } from '@renproject/multiwallet-ui'
-import classNames from 'classnames'
-import React, { FunctionComponent, useCallback, useEffect, useMemo, useState, } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { useWindowSize } from 'react-use'
-import { env } from '../../constants/environmentVariables'
-import { RenNetworkSelector } from '../../features/network/components/NetworkHelpers'
-import { $renNetwork } from '../../features/network/networkSlice'
-import { useSetNetworkFromParam } from '../../features/network/networkUtils'
+import {
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  Grid,
+  ListItem,
+  useTheme,
+} from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import CloseIcon from "@material-ui/icons/Close";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  useMultiwallet,
+  WalletPickerModal,
+  WalletPickerProps,
+} from "@renproject/multiwallet-ui";
+import classNames from "classnames";
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useWindowSize } from "react-use";
+import { env } from "../../constants/environmentVariables";
+import { RenNetworkSelector } from "../../features/network/components/NetworkHelpers";
+import { $renNetwork } from "../../features/network/networkSlice";
+import { useSetNetworkFromParam } from "../../features/network/networkUtils";
+import { TransactionHistory } from "../../features/transactions/TransactionHistory";
 import {
   $transactionsData,
   $transactionsNeedsAction,
   setTxHistoryOpened,
-} from '../../features/transactions/transactionsSlice'
-import { useTestnetName } from '../../features/ui/uiHooks'
-import { $multiwalletChain, $walletPickerOpened, setWalletPickerOpened, } from '../../features/wallet/walletSlice'
-import { walletPickerModalConfig } from '../../providers/multiwallet/Multiwallet'
+} from "../../features/transactions/transactionsSlice";
+import { useTestnetName } from "../../features/ui/uiHooks";
+import {
+  $multiwalletChain,
+  $walletPickerOpened,
+  setWalletPickerOpened,
+} from "../../features/wallet/walletSlice";
+import { walletPickerModalConfig } from "../../providers/multiwallet/Multiwallet";
 import {
   useSelectedChainWallet,
   useSyncMultiwalletNetwork,
   useWallet,
-} from '../../providers/multiwallet/multiwalletHooks'
-import { useWeb3Signatures } from '../../services/web3'
-import { TransactionHistoryMenuIconButton } from '../buttons/Buttons'
-import { RenBridgeLogoIcon } from '../icons/RenIcons'
-import { Debug } from '../utils/Debug'
+} from "../../providers/multiwallet/multiwalletHooks";
+import { useWeb3Signatures } from "../../services/web3";
+import { TransactionHistoryMenuIconButton } from "../buttons/Buttons";
+import { RenBridgeLogoIcon } from "../icons/RenIcons";
+import { Debug } from "../utils/Debug";
 import {
   useWalletPickerStyles,
   WalletConnectingInfo,
   WalletConnectionStatusButton,
   WalletEntryButton,
   WalletWrongNetworkInfo,
-} from '../wallet/WalletHelpers'
-import { Footer } from './Footer'
+} from "../wallet/WalletHelpers";
+import { Footer } from "./Footer";
 
 const headerHeight = 82;
 const footerHeight = 55;
@@ -319,7 +342,7 @@ export const MainLayout: FunctionComponent<MainLayoutProps> = ({
               it={{ debugNetworkName, debugWallet, debugMultiwallet, env }}
             />
           </main>
-          {/*<TransactionHistory />*/}
+          <TransactionHistory />
           <Footer />
         </Container>
       </Grid>
