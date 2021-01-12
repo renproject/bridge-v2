073c487f040f2321fb97c302327007c785207013
Ryan <serjndestroy@gmail.com>
2021-01-12 13:26:16 +0800

    feat(multiwallet): add support for BSC MetaMask
    This allows users to interact with BSC via MetaMask

--- src/providers/multiwallet/Multiwallet.tsx
+++ src/providers/multiwallet/Multiwallet.tsx
@@ -1,8 +1,15 @@
+import Typography from "@material-ui/core/Typography";
 import { RenNetwork } from "@renproject/interfaces";
 import { BinanceSmartChainInjectedConnector } from "@renproject/multiwallet-binancesmartchain-injected-connector";
 import { EthereumInjectedConnector } from "@renproject/multiwallet-ethereum-injected-connector";
 import { MultiwalletProvider as RenMultiwalletProvider } from "@renproject/multiwallet-ui";
 import React, { FunctionComponent } from "react";
+import {
+  ActionButton,
+  ActionButtonWrapper,
+} from "../../components/buttons/Buttons";
+import { PaperContent } from "../../components/layout/Paper";
+import { SpacedPaperContent } from "../../features/transactions/components/TransactionsHelpers";
 import { RenChain } from "../../utils/assetConfigs";
 
 const networkMapping: Record<number, RenNetwork> = {
@@ -40,10 +47,43 @@ export const walletPickerModalConfig = {
       // },
     ],
     [RenChain.binanceSmartChain]: [
+      // FIXME: this will load Metamask if BSC Wallet is not installed
+      // is this behavior what we want?
       {
         name: "BinanceSmartWallet",
         logo: "https://avatars2.githubusercontent.com/u/45615063?s=60&v=4",
         connector: new BinanceSmartChainInjectedConnector({ debug: true }),
+      },
+      // TODO: move this config into its own connector?
+      {
+        name: "Metamask",
+        logo: "https://avatars2.githubusercontent.com/u/45615063?s=60&v=4",
+        info: ({ acknowledge, onClose }: any) => (
+          <>
+            <SpacedPaperContent topPadding>
+              <Typography variant="h5" align="center" gutterBottom>
+                Please ensure that you have added the BSC network to Metamask as
+                explained{" "}
+                <a href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain">
+                  here
+                </a>
+              </Typography>
+            </SpacedPaperContent>
+            <PaperContent>
+              <ActionButtonWrapper>
+                <ActionButton onClick={acknowledge}>Ok</ActionButton>
+                <ActionButton onClick={onClose}>Cancel</ActionButton>
+              </ActionButtonWrapper>
+            </PaperContent>
+          </>
+        ),
+        connector: (() => {
+          const connector = new BinanceSmartChainInjectedConnector({
+            debug: true,
+          });
+          connector.getProvider = () => (window as any).ethereum;
+          return connector;
+        })(),
       },
     ],
   },
