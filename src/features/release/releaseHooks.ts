86d1ccd2bd09e259565255c0b171363f99f2f22f
Ryan <serjndestroy@gmail.com>
2021-01-20 16:17:06 +0800

    fix(fe): network mappings and persistence

--- src/features/release/releaseHooks.ts
+++ src/features/release/releaseHooks.ts
@@ -1,27 +1,31 @@
-import { useMultiwallet } from '@renproject/multiwallet-ui'
-import { burnMachine, BurnMachineSchema, GatewaySession } from '@renproject/ren-tx'
-import { useMachine } from '@xstate/react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { env } from '../../constants/environmentVariables'
-import { db } from '../../services/database/database'
-import { DbGatewaySession } from '../../services/database/firebase/firebase'
-import { getRenJs } from '../../services/renJs'
-import { burnChainMap, releaseChainMap } from '../../services/rentx'
-import { $renNetwork } from '../network/networkSlice'
-import { updateTransaction } from '../transactions/transactionsSlice'
+import { useMultiwallet } from "@renproject/multiwallet-ui";
+import {
+  burnMachine,
+  BurnMachineSchema,
+  GatewaySession,
+} from "@renproject/ren-tx";
+import { useMachine } from "@xstate/react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { env } from "../../constants/environmentVariables";
+import { db } from "../../services/database/database";
+import { DbGatewaySession } from "../../services/database/firebase/firebase";
+import { getRenJs } from "../../services/renJs";
+import { burnChainMap, releaseChainMap } from "../../services/rentx";
+import { $renNetwork } from "../network/networkSlice";
+import { updateTransaction } from "../transactions/transactionsSlice";
 
-export type BurnMachineSchemaState = keyof BurnMachineSchema['states'];
+export type BurnMachineSchemaState = keyof BurnMachineSchema["states"];
 export const useBurnMachine = (burnTransaction: GatewaySession) => {
-  const { enabledChains } = useMultiwallet()
-  const network = useSelector($renNetwork)
+  const { enabledChains } = useMultiwallet();
+  const network = useSelector($renNetwork);
   const providers = Object.entries(enabledChains).reduce(
     (c, n) => ({
       ...c,
       [n[0]]: n[1].provider,
     }),
     {}
-  )
+  );
   return useMachine(burnMachine, {
     context: {
       tx: burnTransaction,
@@ -34,16 +38,16 @@ export const useBurnMachine = (burnTransaction: GatewaySession) => {
       autoSubmit: !!Object.values(burnTransaction.transactions)[0],
     },
     devTools: env.XSTATE_DEVTOOLS,
-  })
-}
+  });
+};
 
 export enum BurnState {
-  restoring = 'restoring',
-  created = 'created',
-  createError = 'createError',
-  srcSettling = 'srcSettling',
-  srcConfirmed = 'srcConfirmed',
-  destInitiated = 'destInitiated', // We only care if the txHash has been issued by renVM
+  restoring = "restoring",
+  created = "created",
+  createError = "createError",
+  srcSettling = "srcSettling",
+  srcConfirmed = "srcConfirmed",
+  destInitiated = "destInitiated", // We only care if the txHash has been issued by renVM
 }
 
 export const releaseTxStateUpdateSequence = [
@@ -51,7 +55,7 @@ export const releaseTxStateUpdateSequence = [
   BurnState.srcSettling,
   BurnState.srcConfirmed,
   BurnState.destInitiated,
-]
+];
 export const shouldUpdateReleaseTx = (
   tx: GatewaySession | DbGatewaySession,
   dbTx: DbGatewaySession,
@@ -59,41 +63,47 @@ export const shouldUpdateReleaseTx = (
 ) => {
   // update when the new state is next in sequence
   // will prevent multiple updates in separate sessions
-  const dbState = dbTx?.meta?.state
+  const dbState = dbTx?.meta?.state;
   if (!dbState) {
     // update when no state
-    return true
+    return true;
   }
   const dbStateIndex = releaseTxStateUpdateSequence.indexOf(
     dbState as BurnState
-  )
-  const stateIndex = releaseTxStateUpdateSequence.indexOf(state as BurnState)
+  );
+  const stateIndex = releaseTxStateUpdateSequence.indexOf(state as BurnState);
   if (stateIndex <= 0) {
     //dont update for created (updated during creation) or not supported states
-    return false
+    return false;
   }
-  return stateIndex > dbStateIndex
-}
+  if (JSON.stringify(dbTx) === JSON.stringify(tx)) {
+    return false;
+  }
+  return (
+    stateIndex > dbStateIndex ||
+    stateIndex == releaseTxStateUpdateSequence.length - 1
+  );
+};
 export const useReleaseTransactionPersistence = (
   tx: GatewaySession | DbGatewaySession,
   state: BurnMachineSchemaState
 ) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     if (!state) {
-      return
+      return;
     }
     db.getTx(tx)
       .then((dbTx) => {
         if (shouldUpdateReleaseTx(tx, dbTx, state)) {
-          const newDbTx = { ...tx, meta: { state } }
+          const newDbTx = { ...tx, meta: { state } };
           db.updateTx(newDbTx).then(() => {
-            dispatch(updateTransaction(newDbTx))
-          })
+            dispatch(updateTransaction(newDbTx));
+          });
         }
       })
       .catch((err) => {
-        console.warn('Release Tx synchronization failed', err)
-      })
-  }, [dispatch, tx, state])
-}
+        console.warn("Release Tx synchronization failed", err);
+      });
+  }, [dispatch, tx, state]);
+};
