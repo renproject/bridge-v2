671f856ea5b7fbc8fd23d23b2d7d5a36be2671cd
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-01-27 20:33:15 +0100

    fix(fe) lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React, { Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AppLoader } from "./components/progress/AppLoader";
-import AboutPage from "./pages/AboutPage";
-import CatalogPage from "./pages/CatalogPage";
-import MainPage from "./pages/MainPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { paths } from "./pages/routes";
 import { WelcomePage } from "./pages/WelcomePage";
 
-// TODO: crit: fix before merge
-// const CatalogPage = lazy(() => import("./pages/CatalogPage"));
-// const MainPage = lazy(() => import("./pages/MainPage"));
-// const AboutPage = lazy(() => import("./pages/AboutPage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage"));
+const MainPage = lazy(() => import("./pages/MainPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
 
 const mainPagePaths = [
   paths.HOME,
