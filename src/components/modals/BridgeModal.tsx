e56561380ab3ccafe26c3a28e7558787101a4609
Ryan <serjndestroy@gmail.com>
2021-02-22 15:50:15 +0800

    fix(deps): bump ren deps

--- src/components/modals/BridgeModal.tsx
+++ src/components/modals/BridgeModal.tsx
@@ -4,7 +4,7 @@ import MuiDialogTitle, {
   DialogTitleProps,
 } from "@material-ui/core/DialogTitle";
 import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, styled } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
 import classNames from "classnames";
