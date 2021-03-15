2a8d731231c70f99e724680dbd131afe05c86b80
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-03-15 12:17:51 +0100

    feat(fe) picking completed transactions

--- src/services/renJs.ts
+++ src/services/renJs.ts
@@ -1,11 +1,16 @@
 import { RenNetwork } from "@renproject/interfaces";
 import RenJS from "@renproject/ren";
+import { RenJSConfig } from "@renproject/ren/build/main/config";
+
+const renJsConfig: RenJSConfig = {
+  loadCompletedDeposits: true,
+};
 
 type RenJSCache = Record<RenNetwork, RenJS>;
 const renJsCache: Partial<RenJSCache> = {};
 export const getRenJs = (network: RenNetwork) => {
   if (!renJsCache[network]) {
-    renJsCache[network] = new RenJS(network);
+    renJsCache[network] = new RenJS(network, renJsConfig);
   }
   return renJsCache[network] as RenJS;
 };
