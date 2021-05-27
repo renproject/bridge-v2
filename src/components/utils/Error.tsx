683c368cfdb1ff9e88029c2908c349c8cb6deaf8
Ryan <serjndestroy@gmail.com>
2021-05-27 18:26:41 +0800

    fix(burn): catch invalid querystring serialization

---
+++ src/components/utils/Error.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+export class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+  constructor(props: any) {
+    super(props);
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+  componentDidCatch(error: Error, errorInfo: any) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1 style={{ padding: "1em", textAlign: "center" }}>
+          Something went wrong.
+        </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
--- src/features/release/ReleaseFlow.tsx
+++ src/features/release/ReleaseFlow.tsx
@@ -1,13 +1,14 @@
-import React, { FunctionComponent, useCallback, useState } from 'react'
-import { RouteComponentProps } from 'react-router'
-import { Route } from 'react-router-dom'
-import { paths } from '../../pages/routes'
-import { usePageTitle } from '../../providers/TitleProviders'
-import { TransactionTypeTabs } from '../transactions/components/TransactionTypeTabs'
-import { TxConfigurationStep } from '../transactions/transactionsUtils'
-import { ReleaseFeesStep } from './steps/ReleaseFeesStep'
-import { ReleaseInitialStep } from './steps/ReleaseInitialStep'
-import { ReleaseProcessStep } from './steps/ReleaseProcessStep'
+import React, { FunctionComponent, useCallback, useState } from "react";
+import { RouteComponentProps } from "react-router";
+import { Route } from "react-router-dom";
+import { ErrorBoundary } from "../../components/utils/Error";
+import { paths } from "../../pages/routes";
+import { usePageTitle } from "../../providers/TitleProviders";
+import { TransactionTypeTabs } from "../transactions/components/TransactionTypeTabs";
+import { TxConfigurationStep } from "../transactions/transactionsUtils";
+import { ReleaseFeesStep } from "./steps/ReleaseFeesStep";
+import { ReleaseInitialStep } from "./steps/ReleaseInitialStep";
+import { ReleaseProcessStep } from "./steps/ReleaseProcessStep";
 
 const ReleaseConfiguration: FunctionComponent<RouteComponentProps> = () => {
   const [step, setStep] = useState(TxConfigurationStep.INITIAL);
@@ -37,12 +38,14 @@ export const ReleaseFlow: FunctionComponent = () => {
   usePageTitle("Releasing");
   return (
     <>
-      <Route exact path={paths.RELEASE} component={ReleaseConfiguration} />
-      <Route
-        exact
-        path={paths.RELEASE_TRANSACTION}
-        component={ReleaseProcessStep}
-      />
+      <ErrorBoundary>
+        <Route exact path={paths.RELEASE} component={ReleaseConfiguration} />
+        <Route
+          exact
+          path={paths.RELEASE_TRANSACTION}
+          component={ReleaseProcessStep}
+        />
+      </ErrorBoundary>
     </>
   );
 };
