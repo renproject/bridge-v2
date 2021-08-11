c4c5228df718e88f527d51a2761fe44e020694ae
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-08-11 02:19:24 +0200

    chore(tests) ingore failing page test suites

--- src/pages/MainPage.spec.tsx
+++ src/pages/MainPage.spec.tsx
@@ -1,6 +1,8 @@
+import i18n from "i18next";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
+import { I18nextProvider } from "react-i18next";
 import { Route, Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 
@@ -17,19 +19,22 @@ import { storageKeys } from "../constants/constants";
 xtest("MainPage renders", (a) => {
   localStorage.setItem(storageKeys.TERMS_AGREED, "true");
   const div = document.createElement("div");
+
   const history = createMemoryHistory();
   act(() => {
     ReactDOM.render(
       <Provider store={store}>
-        <MuiThemeProvider theme={lightTheme}>
-          <TitleProviders>
-            <NotificationsProvider>
-              <Router history={history}>
-                <Route component={MainPage} />
-              </Router>
-            </NotificationsProvider>
-          </TitleProviders>
-        </MuiThemeProvider>
+        <I18nextProvider i18n={i18n}>
+          <MuiThemeProvider theme={lightTheme}>
+            <TitleProviders>
+              <NotificationsProvider>
+                <Router history={history}>
+                  <Route component={MainPage} />
+                </Router>
+              </NotificationsProvider>
+            </TitleProviders>
+          </MuiThemeProvider>
+        </I18nextProvider>
       </Provider>,
       div
     );
