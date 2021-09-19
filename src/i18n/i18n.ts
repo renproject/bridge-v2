258afc2df81bf34dc388599fda699305eb0cd70d
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-09-19 23:56:39 +0200

    feat(i18n) - fallback language for production

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -90,7 +90,7 @@ i18n.init({
   resources: !enableAllTranslations ? defaultResources : undefined,
   lng: !enableAllTranslations ? DEFAULT_LOCALE : undefined, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
   whitelist: availableLocales, // available languages for browser detector to pick from
-  fallbackLng: false,
+  fallbackLng: enableAllTranslations ? false : "en",
   detection: langDetectorOptions,
   // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
   // if you're using a language detector, do not define the lng option
