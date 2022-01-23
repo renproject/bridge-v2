b0e1fedb816325ef288f4f96e67e1b9b269e4dd9
Yoginth <yoginth.saravanan@cred.club>
2022-01-23 09:26:50 +0530

    chore: resolve comments

--- src/components/utils/useENS.ts
+++ src/components/utils/useENS.ts
@@ -3,20 +3,20 @@ import { env } from "../../constants/environmentVariables";
 import { useEffect, useState } from "react";
 
 export function useENS(address: string | null | undefined) {
-  const provider = new ethers.providers.JsonRpcProvider(
-    `https://mainnet.infura.io/v3/${env.INFURA_ID}`
-  );
-  const [ensName, setENSName] = useState<string | null>(null);
+  const [ensName, setEnsName] = useState<string | null>(null);
 
   useEffect(() => {
     async function resolveENS() {
+      const provider = new ethers.providers.JsonRpcProvider(
+        `https://mainnet.infura.io/v3/${env.INFURA_ID}`
+      );
+
       if (address && ethers.utils.isAddress(address)) {
         const ensName = await provider.lookupAddress(address);
-        if (ensName) setENSName(ensName);
+        if (ensName) setEnsName(ensName);
       }
     }
     resolveENS();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address]);
 
   return { ensName };
