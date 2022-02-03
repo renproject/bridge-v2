4e6e20ec8dd3ebf6cef9eb94b23fc86dc421b803
tomasz-rozmus <tomasz.rozmus@gmail.com>
2022-02-03 22:03:24 +0100

    chore: cleaning

--- src/features/network/networkSlice.ts
+++ src/features/network/networkSlice.ts
@@ -1,4 +1,4 @@
-import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RenNetwork } from "@renproject/utils";
 import { env } from "../../constants/environmentVariables";
 import { RootState } from "../../store/rootReducer";
