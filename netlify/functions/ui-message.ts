e4b3fbdeb41d0c6b6897d7c949596ffd563574c4
tomasz-rozmus <tomasz.rozmus@gmail.com>
2022-05-31 17:09:52 +0200

    chore: caching test

--- netlify/functions/ui-message.ts
+++ netlify/functions/ui-message.ts
@@ -1,9 +1,11 @@
 import { Handler } from "@netlify/functions";
 
+const testData = new Date().toISOString();
+
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World " + testData.toString() }),
   };
 };
 
