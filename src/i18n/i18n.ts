ea6c86109c87e70fa7818579d6942bbb4b593fc9
tomasz-rozmus <tomasz.rozmus@gmail.com>
2022-06-07 19:04:50 +0200

    fix: disabling languages

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,6 +3,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 import intervalPlural from "i18next-intervalplural-postprocessor";
 import { initReactI18next } from "react-i18next";
+import { isProductionLike } from "../components/utils/Debug";
 import { getVariationOfAOrAn } from "./i18nUtils";
 import bundles, { availableLocales } from "./localeBundles";
 
@@ -74,8 +75,7 @@ i18n.use(LanguageDetector).use(intervalPlural).use(initReactI18next); // passes 
 
 // if you want to edit language file with HMR make hasTranslations=false and
 // set DEFAULT_LOCALE to edited language key
-export const enableAllTranslations =
-  window.location.hostname !== "bridge.renproject.io";
+export const enableAllTranslations = !isProductionLike;
 
 if (enableAllTranslations) {
   i18n.use(HttpApi);
